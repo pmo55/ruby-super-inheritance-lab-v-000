a4ffaeeb39060f2a5b2b4b11f5567b0a0bf061c1
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-25 20:43:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,8 @@
 class Student
   def hello
-    "Hey there! I'm so excited to learn stuff."
+  puts  "Hey there! I'm so excited to learn stuff."
   end
   def raise_hand
-    "Pck me!"
+  puts   "Pck me!"
   end
 end
